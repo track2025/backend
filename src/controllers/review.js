1ac1d9f4024b9a208f0e4188c73d61c7aa66aac2
Hassan-Dawood <136306696+Hassan-Dawood@users.noreply.github.com>
2024-05-07 13:52:18 +0500

    update review apis

--- src/controllers/review.js
+++ src/controllers/review.js
@@ -1,32 +1,36 @@
-// controllers/ReviewController.js
 const Review = require('../models/Review');
-const { getUser } = require('../config/getUser');
-const blurDataUrl = require("../config/getBlurDataURL");
+const { getUser, getAdmin } = require('../config/getUser');
+
 const getReviews = async (req, res) => {
     try {
-        const reviews = await Review.find({}).populate({
-			path: "user",
-			select: ["firstName", "lastName", "cover", "orders"],
-		});
-        res.status(200).json({ success: true, data: reviews });
+        const skip = 10;
+        const ReviewTotal = await Review.find({}, null, {}).sort({
+            createdAt: -1,
+        });
+        
+        const reviews = await Review.find({}, null, {
+            skip: skip * (page - 1),
+            limit: skip,
+        }).populate({
+            path: "user",
+            select: ["firstName", "lastName", "cover", "orders"],
+        });
+        
+        res.status(200).json({
+            success: true,
+            data: reviews,
+            count: Math.ceil(ReviewTotal.length / skip),
+        });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
 };
 
 const createReview = async (req, res) => {
-   try {
+    try {
         const user = await getUser(req, res);
         const uid = user._id.toString();
-        const { rating, review, images ,designation} = req.body;
-
-        // Process images to generate blurDataURL
-        const updatedImages = await Promise.all(
-            images.map(async (image) => {
-              const blurDataURL = await blurDataUrl(image.url);
-              return { ...image, blurDataURL };
-            })
-          );
+        const { rating, review, designation } = req.body;
 
         // Create new review
         const newReview = await Review.create({
@@ -34,7 +38,6 @@ const createReview = async (req, res) => {
             rating,
             review,
             designation,
-            images: updatedImages
         });
 
         res.status(201).json({ success: true, data: newReview });
@@ -45,8 +48,35 @@ const createReview = async (req, res) => {
 
 const getReviewsByAdmin = async (req, res) => {
     try {
-        const reviews = await Review.find({});
-        res.status(200).json({ success: true, data: reviews });
+        const skip = 10;
+        const ReviewTotal = await Review.find({}, null, {}).sort({
+            createdAt: -1,
+        });
+
+        const page = parseInt(req.query.page) || 1;
+
+        const data = await Review.find({}, null, {
+            skip: skip * (page - 1),
+            limit: skip,
+        }).sort({
+            createdAt: -1,
+        });
+
+        res.status(200).json({
+            success: true,
+            data: data,
+            count: Math.ceil(ReviewTotal.length / skip),
+        });
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+const getOneReviewByAdmin = async (req, res) => {
+    try {
+        const { rid } = req.params;
+        const review = await Review.findById(rid);
+        res.status(200).json({ success: true, data: review });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
@@ -56,15 +86,7 @@ const createReviewByAdmin = async (req, res) => {
     try {
         const admin = await getAdmin(req, res);
         const uid = admin._id.toString();
-        const { rating, review, images ,designation} = req.body;
-
-        // Process images to generate blurDataURL
-        const updatedImages = await Promise.all(
-            images.map(async (image) => {
-              const blurDataURL = await blurDataUrl(image.url);
-              return { ...image, blurDataURL };
-            })
-          );
+        const { rating, review, designation } = req.body;
 
         // Create new review
         const newReview = await Review.create({
@@ -72,7 +94,6 @@ const createReviewByAdmin = async (req, res) => {
             rating,
             review,
             designation,
-            images: updatedImages
         });
 
         res.status(201).json({ success: true, data: newReview });
@@ -80,10 +101,45 @@ const createReviewByAdmin = async (req, res) => {
         res.status(400).json({ success: false, error: error.message });
     }
 };
+
+const updateReviewByAdmin = async (req, res) => {
+    try {
+        const admin = await getAdmin(req, res);
+        const uid = admin._id.toString();
+        const { rid } = req.params;
+        const { rating, review, designation } = req.body;
+
+        const newReview = await Review.findByIdAndUpdate(rid, {
+            user: uid,
+            rating,
+            review,
+            designation,
+        });
+
+        res.status(201).json({ success: true, data: newReview });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+    }
+};
+
+const deleteReviewByAdmin = async (req, res) => {
+    try {
+        const { rid } = req.params;
+
+        const deletedReview = await Review.findByIdAndDelete(rid);
+
+        res.status(201).json({ success: true, data: deletedReview });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+    }
+};
+
 module.exports = {
     getReviews,
     createReview,
     getReviewsByAdmin,
-    createReviewByAdmin
-
-}
+    getOneReviewByAdmin,
+    createReviewByAdmin,
+    updateReviewByAdmin,
+    deleteReviewByAdmin
+};
--- src/routes/review.js
+++ src/routes/review.js
@@ -1,12 +1,18 @@
-// routes/ReviewRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const ReviewController = require('../controllers/review');
 const verifyToken = require("../config/jwt");
 
-router.get('/app-reviews', verifyToken,ReviewController.getReviews);
-router.post('/app-reviews',verifyToken, ReviewController.createReview);
-router.get('/admin/app-reviews',verifyToken, ReviewController.getReviewsByAdmin);
-router.post('/admin/app-reviews',verifyToken, ReviewController.createReviewByAdmin);
+// Routes for regular users (app reviews)
+router.get('/app-reviews', verifyToken, ReviewController.getReviews); // Get all app reviews
+router.post('/app-reviews', verifyToken, ReviewController.createReview); // Create a new app review
+
+// Routes for admin users (app reviews)
+router.get('/admin/app-reviews', verifyToken, ReviewController.getReviewsByAdmin); // Get all app reviews for admin
+router.get('/admin/app-reviews/:rid', verifyToken, ReviewController.getOneReviewByAdmin); // Get a single app review by ID for admin
+router.post('/admin/app-reviews', verifyToken, ReviewController.createReviewByAdmin); // Create a new app review by admin
+router.put('/admin/app-reviews/:rid', verifyToken, ReviewController.updateReviewByAdmin); // Update an existing app review by admin
+router.delete('/admin/app-reviews/:rid', verifyToken, ReviewController.deleteReviewByAdmin); // Delete an existing app review by admin
 
 module.exports = router;
