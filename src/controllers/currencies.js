9508024a021e8ee0f210f49138ce28db5ac28006
James Ishola <brain@shoutty.app>
2025-08-04 06:00:37 +0400

    currency fix

--- src/controllers/currencies.js
+++ src/controllers/currencies.js
@@ -6,34 +6,13 @@ const getAdminCurrencies = async (request, response) => {
 
     const limit = parseInt(limitQuery) || 10;
     const page = parseInt(pageQuery) || 1;
-
-    // Calculate skip correctly
     const skip = limit * (page - 1);
 
-    const currencies = await Currency.aggregate([
-      {
-        $sort: {
-          available: -1,
-        },
-      },
-      {
-        $skip: skip,
-      },
-      {
-        $limit: limit,
-      },
-
-      {
-        $project: {
-          name: 1,
-          code: 1,
-          rate: 1,
-          country: 1,
-          status: 1,
-          createdAt: 1,
-        },
-      },
-    ]);
+    const currencies = await Currency.find()
+      .skip(skip)
+      .limit(limit)
+      .select('name code rate country status createdAt')
+      .lean();
 
     response.status(200).json({
       success: true,
@@ -43,6 +22,8 @@ const getAdminCurrencies = async (request, response) => {
     response.status(400).json({ success: false, message: error.message });
   }
 };
+
+
 const getCurrency = async (request, response) => {
   try {
     const currency = await Currency.findById(request.params.cid);
