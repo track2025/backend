ee7e5f8886b8dc5e98bc68cda05894796d064568
shahadath-shoutty <a.shahadath@shoutty.app>
2025-10-28 20:49:59 +0600

    WIP physical checkout

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,4 +1,5 @@
-const Products = require('../models/Product');
+const PhysicalProduct = require("../models/PhysicalProduct");
+const Products = require("../models/Product");
 
 const getCart = async (request, response) => {
   try {
@@ -6,40 +7,71 @@ const getCart = async (request, response) => {
     const cartItems = [];
 
     for (const item of req.products) {
-      const product = await Products.findById(item.pid).select([
-        'cover',
-        'name',
-        'brand',
-        'slug',
-        'available',
-        'price',
-        'priceSale',
-      ]);
+      console.log(item, "OKKK SEEE");
+      let product;
+      if (item?.checkoutType == "product") {
+        product = await Products.findById(item.pid).select([
+          "cover",
+          "name",
+          "brand",
+          "slug",
+          "available",
+          "price",
+          "priceSale",
+        ]);
+
+        const { quantity, color, size, sku } = item;
+        if (product.available < quantity) {
+          return response
+            .status(400)
+            .json({ success: false, message: "No Products in Stock" });
+        }
+
+        const subtotal = (product.priceSale || product.price) * quantity;
+        const { ...others } = product.toObject();
+        cartItems.push({
+          ...others,
+          pid: item.pid,
+          quantity,
+          size,
+          image: item.image,
+          color,
+          subtotal: subtotal.toFixed(2),
+          sku: sku,
+          checkoutType: item?.checkoutType,
+        });
+      } else if (item?.checkoutType == "physical-product") {
+        console.log("Come here to get chart");
+        product = await PhysicalProduct.findById(item.pid).select([
+          "variants",
+          "images",
+          "name",
+          "brand",
+          "slug",
+          "available",
+          "price",
+          "priceSale",
+        ]);
+
+        const { ...others } = product.toObject();
+        cartItems.push({
+          ...others,
+          pid: item.pid,
+          quantity: 1,
+          size: null,
+          image: item.image,
+          color: null,
+          subtotal: item?.subtotal?.toFixed(2) || 0,
+          sku: item?.sku,
+          checkoutType: item?.checkoutType,
+        });
+      }
 
       if (!product) {
         return response
           .status(404)
-          .json({ success: false, message: 'Products Not Found' });
+          .json({ success: false, message: "Products Not Found" });
       }
-      const { quantity, color, size, sku } = item;
-      if (product.available < quantity) {
-        return response
-          .status(400)
-          .json({ success: false, message: 'No Products in Stock' });
-      }
-
-      const subtotal = (product.priceSale || product.price) * quantity;
-      const { ...others } = product.toObject();
-      cartItems.push({
-        ...others,
-        pid: item.pid,
-        quantity,
-        size,
-        image: item.image,
-        color,
-        subtotal: subtotal.toFixed(2),
-        sku: sku,
-      });
     }
 
     return response.status(200).json({
