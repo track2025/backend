90f21b8609fff7201b3e39f095de82e3648d76eb
DESKTOP-ID4Q3QV\Hanniel <dhanniel21@gmail.com>
2025-11-07 11:58:40 +0100

    Fix: removed validation for cover image && logo

--- src/models/Shop.js
+++ src/models/Shop.js
@@ -11,29 +11,23 @@ const ShopSchema = new mongoose.Schema(
     logo: {
       _id: {
         type: String,
-        required: [true, 'image-id-required-error'],
       },
       url: {
         type: String,
-        required: [true, 'image-url-required-error'],
       },
       blurDataURL: {
         type: String,
-        required: [true, 'image-blur-data-url-required-error'],
       },
     },
     cover: {
       _id: {
         type: String,
-        required: [true, 'image-id-required-error'],
       },
       url: {
         type: String,
-        required: [true, 'image-url-required-error'],
       },
       blurDataURL: {
         type: String,
-        required: [true, 'image-blur-data-url-required-error'],
       },
     },
     username: {
